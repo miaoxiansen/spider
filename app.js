579a34307c40f6b9d1c7c3a9f91abc4d73d0481a
minxiansen <minweinihao@126.com>
2017-07-21 20:00:58 +0800

    add

--- app.js
+++ app.js
@@ -16,25 +16,6 @@ app.get('/', function(req, res) {
 	  });
 	});
 
-var cluster = require('cluster');
-var http = require('http');
-var numCPUs = require('os').cpus().length;
-
-if (cluster.isMaster) {
-    console.log('[master] ' + "start master...");
-
-    for (var i = 0; i < numCPUs; i++) {
-         cluster.fork();
-    }
-
-    cluster.on('listening', function (worker, address) {
-        console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
-    });
-
-} else if (cluster.isWorker) {
-    console.log('[worker] ' + "start worker ..." + cluster.worker.id);
-    http.createServer(function (req, res) {
-        console.log('worker'+cluster.worker.id);
-        res.end('worker'+cluster.worker.id+',PID:'+process.pid);
-    }).listen(3000,"localhost");
-}
+var server = app.listen(3000, function() {
+  console.log('listening at 3000');
+});
