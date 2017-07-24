60ab01000ca06523e6ee179b24e3a61efd08aa87
litao <litaotao356@163.com>
2017-07-24 17:17:48 +0800

    重新设计为10秒之后，开始解析URL抓取数据.

--- app.js
+++ app.js
@@ -3,19 +3,40 @@ var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
 var superagent = require('superagent'); 
+var targetUrl = 'https://team.oschina.net/lyncon';
 
-var targetUrl = 'https://www.oschina.net/';
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
 	  request(targetUrl, function(error, response, body) {
 	    if (!error && response.statusCode == 200) {
 	      $ = cheerio.load(body);
-	      $('span').each(function(){
-	           console.log($(this).text());
+	      $('span').each(function() {
+	      	console.log($(this).text());
 	     });
 	    }
 	  });
+
+	  console.log('正在解析URL中....');
 	});
 
 var server = app.listen(3000, function() {
   console.log('listening at 3000');
-});
+});*/
+
+/*
+ * 启动应用10秒后，开始自动解析URL，抓取数据.
+ */
+(function() {
+	setTimeout(function() {
+		request(targetUrl, function(error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	      $ = cheerio.load(body);
+	      
+	      $('#my_team_tweet_list li').each(function() {
+	      	console.log($(this).text());
+	     });
+	    }
+	  });
+
+	  console.log('正在解析URL中....');
+	}, 10000);
+})();
